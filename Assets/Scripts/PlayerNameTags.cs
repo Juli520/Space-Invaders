10671addda35a20b6c1a38ac41b94bef98942f9e
Juli520 <50522986+Juli520@users.noreply.github.com>
2021-12-05 16:48:19 -0300

    Name Tags

---
+++ Assets/Scripts/PlayerNameTags.cs
@@ -0,0 +1,21 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameTags : MonoBehaviourPun
+{
+    [SerializeField] private TextMeshProUGUI nameText;
+    
+    private void Start()
+    {
+        if(!photonView.IsMine) return;
+
+        photonView.RPC("SetNameRPC", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void SetNameRPC()
+    {
+        nameText.text = photonView.Owner.NickName;
+    }
+}
